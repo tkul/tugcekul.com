2caa67f981160413e47548d393d47329d101207f
tkul <ttugcekul@gmail.com>
2025-03-17 01:29:53 +0300

    refactor: improve animation timing and enhance code formatting for readability

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     gsap.from("h1", { opacity: 0, y: -50, duration: 1 });
-    gsap.from(".social-links a", { opacity: 0, y: 20, stagger: 0.2, duration: 1 });
+    gsap.from(".social-links a", { opacity: 0, y: 20, duration: 1 });
     gsap.from(".project-box", { opacity: 0, y: 50, duration: 1 });
     gsap.from(".project-card", { opacity: 0, y: 50, duration: 1 });
     gsap.from(".about-box", { opacity: 0, y: 50, duration: 1 });
